861e7a845e488f60b6aed93bc892dadfc18e6eec
Chu Tuan An <chutuanan2017@gmail.com>
2025-04-15 14:20:36 +0700

    test

--- src/main/java/com/pms4st/pms/entity/Project.java
+++ src/main/java/com/pms4st/pms/entity/Project.java
@@ -24,7 +24,7 @@ public class Project {
         this.owner = owner;
     }
 
-    // Getters & Setters
+    // Getters & Setters.
     public Long getId() { return id;}
     public void setId(Long id) { this.id = id;}
 
